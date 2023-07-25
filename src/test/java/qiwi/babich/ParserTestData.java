f218e40b38dbcad9e1f58a323202b57f6a179a6e
Kirill Babich <gonertheowner@gmail.com>
2023-07-25 14:42:45 +0300

    console parser test

---
+++ src/test/java/qiwi/babich/ParserTestData.java
@@ -0,0 +1,13 @@
+package qiwi.babich;
+
+public class ParserTestData {
+
+    public static final String validCurrencyCode = "USD";
+    public static final String validCurrencyDateString = "2022-10-08";
+    public static final String invalidCurrencyCode = "invalid code";
+    public static final String invalidCurrencyDateString = "5000-01-01";
+    public static final String currencyCodeOption = "--code=";
+    public static final String dateOption = "--date=";
+    public static final String illegalOption = "--get-free-money=";
+    public static final Integer illegalOptionValue = 1_000_000;
+}
